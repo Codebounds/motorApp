bf0f8a8a3e15be913e04f4e16741d080444a592d
Codeboudns <codebounds@gmail.com>
2020-07-03 11:18:00 +0200

    change Port to 5000

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const persona = require('./Persona/persona');
+const PORT = process.env.PORT || 5000
 
 
-persona.listen(3000, '0.0.0.0', () => {
+persona.listen(PORT, '0.0.0.0', () => {
     console.log('listening to persona');
 })
