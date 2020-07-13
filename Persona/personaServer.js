ae7f7a9c7ce32def99d0a83cc7ae4b3f28a74bb8
Codeboudns <codebounds@gmail.com>
2020-07-14 00:20:29 +0200

    error message

--- Persona/personaServer.js
+++ Persona/personaServer.js
@@ -105,7 +105,7 @@ api.post('/getPersonaFromCedula', (req, res) =>{
                     };
                     res.json({respuesta}).status(200);
                 }else {
-                    res.json({"reason":"No hay usua"})
+                    res.json({"reason":"No existe usuario asociado a este número de cédula"}).status(400);
                 }
             })
             .catch(err => {
